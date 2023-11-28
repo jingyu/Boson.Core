fb1cb8a0c1223b11f7070ab987b932850553a08f
Jingyu <jingyu.niu@gmail.com>
2023-11-28 15:25:56 +0800

    Update the copyright header

--- accesscontrol/src/test/java/io/bosonnetwork/access/impl/PermissionTests.java
+++ accesscontrol/src/test/java/io/bosonnetwork/access/impl/PermissionTests.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2022 - 2023 trinity-tech.io
+ * Copyright (c) 2023 -      bosonnetwork.io
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
